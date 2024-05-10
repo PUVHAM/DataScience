1c121de44c5c0406185879842a3089e0a15dc1fe
PUVHAM <phamquangvu19082005@gmail.com>
2024-05-09 20:56:36 -0700

    Nop bai tap buoi 3

--- Module 2/Buoi 2/MMC-SQL_Lab_2.sql
+++ Module 2/Buoi 2/MMC-SQL_Lab_2.sql
@@ -1,19 +1,27 @@
-DROP DATABASE IF EXISTS TestingSystem;
-CREATE DATABASE TestingSystem;
-USE TestingSystem;
+/*============================== CREATE DATABASE =======================================*/
+/*======================================================================================*/
+DROP DATABASE IF EXISTS testing_system_db;
+CREATE DATABASE testing_system_db;
+USE testing_system_db;
 
+/*============================== CREATE TABLE=== =======================================*/
+/*======================================================================================*/
+
+-- create table 1: Department
 DROP TABLE IF EXISTS Department;
 CREATE TABLE Department(
 	DepartmentID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     DepartmentName 			NVARCHAR(30) NOT NULL UNIQUE KEY
 );
 
+-- create table 2: Posittion
 DROP TABLE IF EXISTS Position;
 CREATE TABLE `Position` (
 	PositionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     PositionName			ENUM('Dev','Test','Scrum Master','PM') NOT NULL UNIQUE KEY
 );
 
+-- create table 3: Account
 DROP TABLE IF EXISTS `Account`;
 CREATE TABLE `Account`(
 	AccountID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -23,40 +31,45 @@ CREATE TABLE `Account`(
     DepartmentID 			TINYINT UNSIGNED NOT NULL,
     PositionID				TINYINT UNSIGNED NOT NULL,
     CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY (DepartmentID) REFERENCES Department (DepartmentID),
-    FOREIGN KEY (PositionID) REFERENCES `Position` (PositionID)
+    FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID),
+    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID)
 );
 
+-- create table 4: Group
 DROP TABLE IF EXISTS `Group`;
 CREATE TABLE `Group`(
 	GroupID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     GroupName				NVARCHAR(50) NOT NULL UNIQUE KEY,
     CreatorID				TINYINT UNSIGNED,
     CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY (CreatorID) 	REFERENCES `Account` (AccountId)
+    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId)
 );
 
+-- create table 5: GroupAccount
 DROP TABLE IF EXISTS GroupAccount;
 CREATE TABLE GroupAccount(
 	GroupID					TINYINT UNSIGNED NOT NULL,
     AccountID				TINYINT UNSIGNED NOT NULL,
     JoinDate				DATETIME DEFAULT NOW(),
-    PRIMARY KEY (GroupID, AccountID),
-    FOREIGN KEY (GroupID) 		REFERENCES `Group` (GroupID) 
+    PRIMARY KEY (GroupID,AccountID),
+    FOREIGN KEY(GroupID) 		REFERENCES `Group`(GroupID) 
 );
 
+-- create table 6: TypeQuestion
 DROP TABLE IF EXISTS TypeQuestion;
 CREATE TABLE TypeQuestion (
     TypeID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    TypeName 		ENUM('Essay','Multiple-Choice') NOT NULL
+    TypeName 		ENUM('Essay','Multiple-Choice') NOT NULL UNIQUE KEY
 );
 
+-- create table 7: CategoryQuestion
 DROP TABLE IF EXISTS CategoryQuestion;
 CREATE TABLE CategoryQuestion(
     CategoryID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     CategoryName			NVARCHAR(50) NOT NULL UNIQUE KEY
 );
 
+-- create table 8: Question
 DROP TABLE IF EXISTS Question;
 CREATE TABLE Question(
     QuestionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -65,20 +78,22 @@ CREATE TABLE Question(
     TypeID					TINYINT UNSIGNED NOT NULL,
     CreatorID				TINYINT UNSIGNED NOT NULL,
     CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion (CategoryID),
-    FOREIGN KEY (TypeID) 		REFERENCES TypeQuestion (TypeID),
-    FOREIGN KEY (CreatorID) 	REFERENCES `Account` (AccountId) 
+    FOREIGN KEY(CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY(TypeID) 		REFERENCES TypeQuestion(TypeID),
+    FOREIGN KEY(CreatorID) 		REFERENCES `Account`(AccountId) 
 );
 
+-- create table 9: Answer
 DROP TABLE IF EXISTS Answer;
 CREATE TABLE Answer(
     AnswerID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Content					NVARCHAR(100) NOT NULL,
     QuestionID				TINYINT UNSIGNED NOT NULL,
     isCorrect				BIT DEFAULT 1,
-    FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID)
 );
 
+-- create table 10: Exam
 DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam(
     ExamID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -88,26 +103,32 @@ CREATE TABLE Exam(
     Duration				TINYINT UNSIGNED NOT NULL,
     CreatorID				TINYINT UNSIGNED NOT NULL,
     CreateDate				DATETIME DEFAULT NOW(),
-    FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID),
-    FOREIGN KEY (CreatorID) 	REFERENCES `Account` (AccountId)
+    FOREIGN KEY(CategoryID) REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId)
 );
 
+-- create table 11: ExamQuestion
 DROP TABLE IF EXISTS ExamQuestion;
 CREATE TABLE ExamQuestion(
     ExamID				TINYINT UNSIGNED NOT NULL,
 	QuestionID			TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID),
-    FOREIGN KEY (ExamID) REFERENCES Exam (ExamID),
-    PRIMARY KEY (ExamID, QuestionID)
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID),
+    FOREIGN KEY(ExamID) REFERENCES Exam(ExamID) ,
+    PRIMARY KEY (ExamID,QuestionID)
 );
 
 INSERT INTO `Department`(`DepartmentName`)
-VALUES                  (N'Lễ tân'),
-                        (N'Bảo vệ'),
-                        (N'Kế toán'),
-                        (N'Nhân sự'),
-                        (N'Kinh doanh'),
-                        (N'Kỹ thuật');
+VALUES                  (N'Lễ tân'		),
+                        (N'Bảo vệ'		),
+                        (N'Kế toán'		),
+                        (N'Sale'		),
+                        (N'Kinh doanh'	),
+                        (N'Kỹ thuật'	),
+						(N'Tài chính'	),
+						(N'Phó giám đốc'),
+						(N'Giám đốc'	),
+						(N'Thư kí'		),
+						(N'Bán hàng'	);
 
     
 -- Add data position
@@ -119,82 +140,106 @@ VALUES 					('Dev'			),
 
 
 -- Add data Account
-INSERT INTO Account (Email, Username, FullName, DepartmentID, PositionID)
+INSERT INTO Account (Email, Username, FullName, DepartmentID, PositionID, CreateDate)
 VALUES
-    ('account01@gmail.com', 'account01', 'Trần Văn A', 1, 1),
-    ('account02@gmail.com', 'account02', 'Nguyễn Thị B', 3, 2),
-    ('account03@gmail.com', 'account03', 'Lê Văn C', 2, 3),
-    ('account04@gmail.com', 'account04', 'Phạm Ti D', 3, 4),
-    ('account05@gmail.com', 'account05', 'Tôn Hoàng E', 1, 1);
+    ('account01@gmail.com'				, 'account01'		, 'Trần Văn A'			, '1'				, '1'			, '2020-03-05'),
+    ('account02@gmail.com'				, 'account02'		, 'Nguyễn Thị B'		, '3'				, '2'			, '2020-06-05'),
+    ('account03@gmail.com'				, 'account03'		, 'Lê Văn C'			, '2'				, '3'			, '2020-03-09'),
+    ('account04@gmail.com'				, 'account04'		, 'Duong Do'			, '3'				, '4'			, '2020-04-15'),
+    ('account05@gmail.com'				, 'account05'		, 'Tôn Hoàng E'			, '1'				, '1'			, '2020-03-07'),
+	('dapphatchetngay@gmail.com'		, 'khabanh'			,'Ngo Ba Kha'			,   '2'				,   '3'			,'2020-04-05'),
+	('songcodaoly@gmail.com'			, 'huanhoahong'		,'Bui Xuan Huan'		,   '1'				,   '2'			, 	NULL	),
+	('sontungmtp@gmail.com'				, 'tungnui'			,'Nguyen Thanh Tung'	,   '2'				,   '1'			,'2020-04-07'),
+	('duongghuu@gmail.com'				, 'duongghuu'		,'Duong Van Huu'		,   '1'				,   '2'			,'2020-04-07'),
+	('vtiaccademy@gmail.com'			, 'vtiaccademy'		,'Vi Ti Ai'				,   '3'				,   '1'			,'2020-04-09');
 
 -- Add data Group
-INSERT INTO `Group`(`GroupName`, `CreatorID`)
-VALUES             ('Nhóm Dẫn đầu', 1),
-                   ('Nhóm Bứt phá', 2),
-                   ('Nhóm Bền bỉ', 3),
-                   ('Nhóm Cẩn thận', 4),
-                   ('Nhóm Tốc độ', 5);
+INSERT INTO `Group`	(  GroupName			, CreatorID		, CreateDate)
+VALUES             	(N'Nhóm Dẫn đầu'		, 1				,'2020-03-24'),
+					(N'Nhóm Bứt phá'		, 2				,'2020-03-26'),
+					(N'Nhóm Bền bỉ'			, 3				,'2020-03-25'),
+					(N'Nhóm Cẩn thận'		, 4				,'2020-03-27'),
+					(N'Nhóm Tốc độ'			, 5				,'2020-03-23'),
+					(N'VTI Sale 03'			,   4			,'2020-03-28'),
+                    (N'VTI Creator'			,   6			,'2020-04-06'),
+                    (N'VTI Marketing 01'	,   7			,'2020-04-07'),
+                    (N'Management'			,   8			,'2020-04-08'),
+                    (N'Chat with love'		,   9			,'2020-04-09'),
+                    (N'Vi Ti Ai'			,   10			,'2020-04-10');
 
-INSERT INTO `GroupAccount`(`GroupID`, `AccountID`)
-VALUES                    (1, 1),
-                          (1, 3),
-                          (2, 2),
-                          (2, 5),
-                          (4, 4),
-                          (4, 1),
-                          (3, 5);
+INSERT INTO `GroupAccount`	(  GroupID	, AccountID	, JoinDate	 )
+VALUES 						(	1		,    1		,'2019-03-05'),
+							(	1		,    2		,'2020-03-07'),
+							(	3		,    3		,'2020-03-09'),
+							(	3		,    4		,'2020-03-10'),
+							(	5		,    5		,'2020-03-28'),
+							(	1		,    3		,'2020-04-06'),
+							(	1		,    7		,'2020-04-07'),
+							(	8		,    3		,'2020-04-08'),
+							(	1		,    9		,'2020-04-09'),
+							(	10		,    10		,'2020-04-10');
 
 
-INSERT INTO `TypeQuestion`(`TypeName`)
-VALUES                    ('Essay'),
-                          ('Essay'),
-                          ('Essay'),
-                          ('Essay');
+INSERT INTO `TypeQuestion`	(`TypeName`)
+VALUES                    	('Essay'),
+							('Multiple-Choice');
 
-INSERT INTO `CategoryQuestion`(`CategoryName`)
-VALUES                        ('Kiến thức chung'),
-                              ('Ngôn ngữ lập trình'),
-                              ('CSDL'),
-                              ('Thuật toán');
+INSERT INTO `CategoryQuestion`	(`CategoryName`)
+VALUES                        	('Postman'),
+								('JAVA'),
+								('ADO.NET'),
+								('ASP.NET'),
+                                ('SQL'			),
+								('Ruby'			),
+								('Python'		),
+								('C++'			),
+								('C Sharp'		),
+								('PHP'			);
 
-INSERT INTO `Question`(`Content`, `CategoryID`, `TypeID`, `CreatorID`)
-VALUES                ('Câu hỏi số 1 là', 1, 1, 1),
-                      ('Câu hỏi số 2 là', 3, 2, 2),
-                      ('Câu hỏi số 3 là', 4, 3, 2),
-                      ('Câu hỏi số 4 là', 2, 4, 1),
-                      ('Câu hỏi số 5 là', 1, 2, 1),
-                      ('Câu hỏi số 6 là', 4, 1, 1),
-                      ('Câu hỏi số 7 là', 3, 4, 3),
-                      ('Câu hỏi số 8 là', 2, 3, 4),
-                      ('Câu hỏi số 9 là', 2, 1, 5),
-                      ('Câu hỏi số 10 là', 1, 1, 2),
-                      ('Câu hỏi số 11 là', 4, 2, 3),
-                      ('Câu hỏi số 12 là', 3, 3, 1);
+INSERT INTO Question	(Content			, CategoryID, TypeID		, CreatorID	, CreateDate )
+VALUES 					(N'Câu hỏi về Java'	,	1		,   '1'			,   '2'		,'2020-04-05'),
+						(N'Câu Hỏi về PHP'	,	10		,   '2'			,   '2'		,'2020-04-05'),
+						(N'Hỏi về C#'		,	9		,   '2'			,   '3'		,'2020-04-06'),
+						(N'Hỏi về Ruby'		,	6		,   '1'			,   '4'		,'2020-04-06'),
+						(N'Hỏi về Postman'	,	5		,   '1'			,   '5'		,'2020-04-06'),
+						(N'Hỏi về ADO.NET'	,	3		,   '2'			,   '6'		,'2020-04-06'),
+						(N'Hỏi về ASP.NET'	,	2		,   '1'			,   '7'		,'2020-04-06'),
+						(N'Hỏi về C++'		,	8		,   '1'			,   '8'		,'2020-04-07'),
+						(N'Hỏi về SQL'		,	4		,   '2'			,   '9'		,'2020-04-07'),
+						(N'Hỏi về Python'	,	7		,   '1'			,   '10'	,'2020-04-07');
 
-INSERT INTO `Answer`(`Content`, `QuestionID`)
-VALUES              ('Đáp án 1 là', 1),
-                    ('Đáp án 2 là', 2),
-                    ('Đáp án 3 là', 3),
-                    ('Đáp án 4 là', 4),
-                    ('Đáp án 5 là', 5),
-                    ('Đáp án 6 là', 6),
-                    ('Đáp án 7 là', 7),
-                    ('Đáp án 8 là', 8),
-                    ('Đáp án 9 là', 9),
-                    ('Đáp án 10 là', 10),
-                    ('Đáp án 11 là', 11),
-                    ('Đáp án 12 là', 12);
+INSERT INTO Answer	(  Content		, QuestionID	, isCorrect	)
+VALUES 				(N'Trả lời 01'	,   1			,	0		),
+					(N'Trả lời 02'	,   1			,	1		),
+                    (N'Trả lời 03'	,   1			,	0		),
+                    (N'Trả lời 04'	,   1			,	1		),
+                    (N'Trả lời 05'	,   2			,	1		),
+                    (N'Trả lời 06'	,   3			,	1		),
+                    (N'Trả lời 07'	,   4			,	0		),
+                    (N'Trả lời 08'	,   8			,	0		),
+                    (N'Trả lời 09'	,   9			,	1		),
+                    (N'Trả lời 10'	,   10			,	1		);
 
-INSERT INTO `Exam`(`Code`, `Title`, `CategoryID`, `Duration`, `CreatorID`)
-VALUES            ('E001', 'Entrance exam', 1, 60, 1),
-                  ('E002', 'Short Entrance exam', 1, 30, 1),
-                  ('E003', 'Evaluation exam', 2, 120, 2),
-                  ('E004', 'Short Evaluation exam', 3, 90, 1),
-                  ('E005', 'Regular exam', 4, 30, 1);
+INSERT INTO Exam	(`Code`			, Title					, CategoryID	, Duration	, CreatorID		, CreateDate )
+VALUES 				('VTIQ001'		, N'Đề thi C#'			,	1			,	60		,   '5'			,'2019-04-05'),
+					('VTIQ002'		, N'Đề thi PHP'			,	10			,	60		,   '2'			,'2019-04-05'),
+                    ('VTIQ003'		, N'Đề thi C++'			,	9			,	120		,   '2'			,'2019-04-07'),
+                    ('VTIQ004'		, N'Đề thi Java'		,	6			,	60		,   '3'			,'2020-04-08'),
+                    ('VTIQ005'		, N'Đề thi Ruby'		,	5			,	120		,   '4'			,'2020-04-10'),
+                    ('VTIQ006'		, N'Đề thi Postman'		,	3			,	60		,   '6'			,'2020-04-05'),
+                    ('VTIQ007'		, N'Đề thi SQL'			,	2			,	60		,   '7'			,'2020-04-05'),
+                    ('VTIQ008'		, N'Đề thi Python'		,	8			,	60		,   '8'			,'2020-04-07'),
+                    ('VTIQ009'		, N'Đề thi ADO.NET'		,	4			,	90		,   '9'			,'2020-04-07'),
+                    ('VTIQ010'		, N'Đề thi ASP.NET'		,	7			,	90		,   '10'		,'2020-04-08');
 
-INSERT INTO `ExamQuestion`(`ExamID`, `QuestionID`)
-VALUES                    (1, 4),
-                          (2, 5),
-                          (3, 7),
-                          (4, 9),
-                          (5, 1);
+INSERT INTO ExamQuestion(ExamID	, QuestionID	) 
+VALUES 					(	1	,		5		),
+						(	2	,		10		), 
+						(	3	,		4		), 
+						(	4	,		3		), 
+						(	5	,		7		), 
+						(	6	,		10		), 
+						(	7	,		2		), 
+						(	8	,		10		), 
+						(	9	,		9		), 
+						(	10	,		8		); 
